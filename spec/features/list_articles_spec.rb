7712ba314d5f21c0030044575a4a705fd5a02d95
Sammy Dallal <sammydallal@gmail.com>
2016-08-14 22:38:12 -0700

    removed links for non-signed in users

--- spec/features/list_articles_spec.rb
+++ spec/features/list_articles_spec.rb
@@ -10,12 +10,12 @@ RSpec.feature "Listing Articles" do
 	scenario "List all articles" do
 		visit "/"
 
-
 		expect(page).to have_content(@article1.title)
 		expect(page).to have_content(@article1.description)
 		expect(page).to have_content(@article2.title)
 		expect(page).to have_content(@article2.description)
 		expect(page).to have_link(@article1.title)
 		expect(page).to have_link(@article2.title)
+		expect(page).not_to have_link("New Article")
 	end
 end
